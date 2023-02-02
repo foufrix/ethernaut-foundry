9f6b40ab18a2cb1a56445ab154d32921d70ebe3c
Raphael ABADIE <rafa.abadie@gmail.com>
2023-02-02 22:08:41 +0800

    add level2

---
+++ test/Level02.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Level02.sol";
+
+contract Level02Test is Test {
+    Level02 public level02;
+
+    function setUp() public {
+      vm.prank(vm.addr(1));
+      level02 = new Level02();
+    }
+
+    function testLevel02() public {
+
+    }
+}
